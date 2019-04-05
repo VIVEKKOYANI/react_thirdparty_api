8860922e60a7c1955acd529c5c9893a340fbc170
vivek <vivek.koyani@gmail.com>
2019-04-05 18:07:51 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-template-curly-in-string */
+import React  from 'react';
+import './App.scss';
+import Collapsible from './Collapsible';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            isLoading: true,
+            contacts: []
+        }
+        
+    }
+    componentDidMount(){
+        this.fetchData();
+    }
+    fetchData(){
+        fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+        .then(response => response.json())
+        .then(parsedJSON => parsedJSON.results.map(user =>  (
+            
+            {
+                
+                name: `${user.name.first} ${user.name.last}`,
+                username: `${user.login.username}`,
+                email: `${user.email}`,
+                location: `${user.location.street}, ${user.location.city}`
+
+            }
+        )))
+        .then(contacts => this.setState({
+            contacts,
+            isLoading: false
+        }))
+        .catch(error => console.log('parsing failed', error))
+        }
+       
+    render(){
+        const {isLoading, contacts} = this.state;
+        return(
+            <div>
+                <header>
+                    <h1>
+                        Fetching Data
+                        <button className="btn btn-sm btn-danger">Fetch now</button>
+                    </h1>
+                </header>
+                <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+                <div className="panel-group">
+                    {
+                        !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                            const {username,name,email,location} = contact;
+                            return <Collapsible key={username}  title={name}>
+                            <p>{email}<br/>{location}</p>
+                            </Collapsible>
+                        }) : null
+                    }
+
+                </div>
+                <div className="loader">
+                <div className="icon"></div>
+                </div>
+                </div>
+               
+                </div>
+        );
+    }
+}
+
+export default App
